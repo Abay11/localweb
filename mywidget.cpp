420bfc6f0cfa7e3802a4c75654f345a1b41fd2ca
J.A.R.V.I.S <adygha@J.A.R.V.I.S>
2018-09-14 22:02:33 +0300

    MyWidget: +GUI

--- mywidget.cpp
+++ mywidget.cpp
@@ -1,9 +1,29 @@
 #include "mywidget.h"
 
 MyWidget::MyWidget(QWidget *parent)
- : QWidget(parent)
+ :QWidget(parent)
+ ,plblPort(new QLabel("Номер порта:"))
+ ,plePort(new QLineEdit("7165"))
+ ,pInfo(new QTextEdit)
+ ,pcmdOn(new QPushButton("Пуск"))
+ ,pcmdOff(new QPushButton("Стоп"))
+ ,phlay(new QHBoxLayout)
+ ,pvlay(new QVBoxLayout)
 {
+ pInfo->setText(QDateTime::currentDateTime().toString("[hh:mm:ss] ")+"Сервер не запущен.");
+ pInfo->setReadOnly(true);
+
+ phlay->addWidget(plblPort);
+ phlay->addWidget(plePort);
+ phlay->addWidget(pcmdOn);
+ phlay->addWidget(pcmdOff);
+
+ pvlay->addLayout(phlay);
+ pvlay->addWidget(pInfo);
+
+ setLayout(pvlay);
 }
+
 
 MyWidget::~MyWidget()
 {
--- mywidget.h
+++ mywidget.h
@@ -2,13 +2,28 @@
 #define MYWIDGET_H
 
 #include <QWidget>
+#include <QLabel>
+#include <QLineEdit>
+#include <QTextEdit>
+#include <QPushButton>
+#include <QHBoxLayout>
+#include <QVBoxLayout>
+#include <QDateTime>
 
 class MyWidget : public QWidget
 {
  Q_OBJECT
+private:
+ QLabel* plblPort;
+ QLineEdit* plePort;
+ QTextEdit* pInfo;
+ QPushButton* pcmdOn;
+ QPushButton* pcmdOff;
+ QHBoxLayout* phlay;
+ QVBoxLayout* pvlay;
 
 public:
- MyWidget(QWidget *parent = 0);
+ MyWidget(QWidget* parent=nullptr);
  ~MyWidget();
 };
 

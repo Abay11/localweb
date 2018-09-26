b29a4cb81182b6336b831198bc6771aec8ec2325
abay11 <adygha@J.A.R.V.I.S>
2018-09-26 21:46:22 +0300

    added basic logic to Registation

---
+++ LocalwebClient/registration.h
@@ -0,0 +1,12 @@
+#ifndef REGISTRATION_H
+#define REGISTRATION_H
+
+#include <QWidget>
+
+class Registration
+{
+public:
+ Registration();
+};
+
+#endif // REGISTRATION_H
---
+++ LocalwebClient/stackedwidget.h
@@ -0,0 +1,11 @@
+#ifndef STACKEDWIDGET_H
+#define STACKEDWIDGET_H
+
+
+class StackedWidget
+{
+public:
+ StackedWidget();
+};
+
+#endif // STACKEDWIDGET_H

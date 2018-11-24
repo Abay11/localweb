091d44ea26e636396278ff2a635187a8182165cc
abay11 <adygha@J.A.R.V.I.S>
2018-11-24 21:28:07 +0300

    TESTS:
    serverNetworkService:
    +5 new tests

--- TestsLocalWeb/TestsLocalWeb.pro
+++ TestsLocalWeb/TestsLocalWeb.pro
@@ -1,9 +1,22 @@
-QT += testlib
-QT -= gui
-
+QT += testlib network widgets
 CONFIG += qt console warn_on depend_includepath testcase
 CONFIG -= app_bundle
 
 TEMPLATE = app
 
-SOURCES +=  tst_test.cpp
+SOURCES +=  tst_test.cpp \
+#client
+../LocalwebClient/clientservice.cpp \
+../clientinfo.cpp \
+#server
+../mywidget.cpp \
+../servernetworkservice.cpp
+
+
+HEADERS += \
+#client
+../LocalwebClient/clientservice.h \
+  ../clientinfo.h \
+#server
+../mywidget.h \
+../servernetworkservice.h
--- TestsLocalWeb/tst_test.cpp
+++ TestsLocalWeb/tst_test.cpp
@@ -1,33 +1,51 @@
 #include <QtTest>
 
 // add necessary includes here
+#include "../LocalwebClient/clientservice.h"
+#include "../mywidget.h"
+#include "../servernetworkservice.h"
 
 class test : public QObject
 {
  Q_OBJECT
 
-public:
- test();
- ~test();
-
 private slots:
- void test_case1();
-
+ void test_startServer0();
+ void test_startServer1();
+ void test_startServer2();
 };
 
-test::test()
+void test::test_startServer0()
 {
-
+ quint16 nPort=7777;
+ ServerNetworkService *pservice=new ServerNetworkService(nPort);
+ QCOMPARE(pservice->slotStartServer(), true);
+ QCOMPARE(pservice->serverPort(), nPort);
+ pservice->slotStopServer();
 }
 
-test::~test()
+void test::test_startServer1()
 {
+ quint16 nPort=7777;
+ ServerNetworkService *pservice0=new ServerNetworkService(nPort);
+ QCOMPARE(pservice0->slotStartServer(), true);
 
+ ServerNetworkService *pservice1=new ServerNetworkService(nPort);
+ QCOMPARE(pservice1->slotStartServer(), false);
+
+ pservice0->slotStopServer();
+ pservice1->slotStopServer();
 }
 
-void test::test_case1()
+void test::test_startServer2()
 {
+ quint16 nPort=8888;
+ ServerNetworkService *pservice=new ServerNetworkService(nPort);
+ pservice->slotStartServer();
+ pservice->slotStopServer();
 
+ QCOMPARE(pservice->slotStartServer(), true);
+ pservice->slotStopServer();
 }
 
 QTEST_APPLESS_MAIN(test)

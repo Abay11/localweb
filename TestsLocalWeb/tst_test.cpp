1a9830e06b89f8a49075fee64f5d5ee23d6c1592
abay11 <adygha@J.A.R.V.I.S>
2018-11-28 20:35:16 +0300

    2 new tests
    for adding to base, saving and restoring data

--- TestsLocalWeb/tst_test.cpp
+++ TestsLocalWeb/tst_test.cpp
@@ -1,19 +1,85 @@
 #include <QtTest>
+#include <QCoreApplication>
 
 // add necessary includes here
 #include "../LocalwebClient/clientservice.h"
 #include "../mywidget.h"
 #include "../servernetworkservice.h"
+#include "simpleclient.h"
+
+#include <QObject>
+#include <unistd.h>
 
 class test : public QObject
 {
  Q_OBJECT
 
 private slots:
+ void test_addToBase();
+ void test_saveDataAndRestoreData();
  void test_startServer0();
  void test_startServer1();
  void test_startServer2();
+// void test_readingMessage();
 };
+
+void test::test_addToBase()
+{
+ ServerNetworkService *pservice=new ServerNetworkService(0);
+ QCOMPARE(pservice->addToBase("nick1","","",""), true);
+ QCOMPARE(pservice->addToBase("nick2","","",""), true);
+ QCOMPARE(pservice->addToBase("JOHNY","","",""), true);
+ QCOMPARE(pservice->addToBase("test00","name","ip","port"), true);
+ QCOMPARE(pservice->addToBase("test0","name","ip","port"), true);
+ QCOMPARE(pservice->addToBase("test","name","ip","port"), true);
+
+ QCOMPARE(pservice->addToBase("nick1","","",""), false);
+ QCOMPARE(pservice->addToBase("NICK1","","",""), false);
+ QCOMPARE(pservice->addToBase("JOHNY","","",""), false);
+ QCOMPARE(pservice->addToBase("test00","name","ip","port"), false);
+ QCOMPARE(pservice->addToBase("test00","","",""), false);
+
+ delete pservice;
+}
+
+void test::test_saveDataAndRestoreData()
+{
+ QString filename="clientdata.bin";
+ quint16 nport=9009;
+ ServerNetworkService *pserviceForSave=new ServerNetworkService(nport);
+ ServerNetworkService *pserviceForRestore=new ServerNetworkService(0);
+
+ pserviceForSave->addToBase("nick1","name1","127.0.0.1","8888");
+ pserviceForSave->addToBase("nick2","name2","127.0.0.1","9999");
+ QCOMPARE(pserviceForSave->saveData(filename), true);
+
+ QCOMPARE(pserviceForRestore->restoreData(filename), true);
+ QCOMPARE(pserviceForRestore->expectedPort(), nport);
+
+ QCOMPARE(pserviceForRestore->getClientBase()->size(), 2);
+
+ auto keys=pserviceForRestore->getClientBase()->keys();
+ QCOMPARE(keys.at(0), "nick1");
+ QCOMPARE(keys.at(1), "nick2");
+
+ auto cinfo1=pserviceForRestore->getClientBase()->first();
+ QCOMPARE(cinfo1->fullName(), "name1");
+ QCOMPARE(cinfo1->address(), "127.0.0.1");
+ QCOMPARE(cinfo1->port(), "8888");
+
+ auto cinfo2=pserviceForRestore->getClientBase()->last();
+ QCOMPARE(cinfo2->fullName(), "name2");
+ QCOMPARE(cinfo2->address(), "127.0.0.1");
+ QCOMPARE(cinfo2->port(), "9999");
+
+
+ QFile file(filename);
+ QCOMPARE(file.exists(), true);
+ file.remove();
+
+ delete pserviceForSave;
+ delete pserviceForRestore;
+}
 
 void test::test_startServer0()
 {
@@ -22,6 +88,8 @@ void test::test_startServer0()
  QCOMPARE(pservice->slotStartServer(), true);
  QCOMPARE(pservice->serverPort(), nPort);
  pservice->slotStopServer();
+
+ delete pservice;
 }
 
 void test::test_startServer1()
@@ -47,6 +115,22 @@ void test::test_startServer2()
  QCOMPARE(pservice->slotStartServer(), true);
  pservice->slotStopServer();
 }
+
+//void test::test_readingMessage()
+//{
+// QString sendingText="Read me!";
+// quint16 nPort=9985;
+
+// ServerNetworkService *pservice=new ServerNetworkService(nPort);
+// if(!pservice->slotStartServer())
+//	QFAIL("Can't open the port");
+
+// SimpleClient *client=new SimpleClient(nPort);
+// client->sendMessage(sendingText);
+
+//// QCOMPARE(pservice->receivedMsg, sendingText);
+// pservice->slotStopServer();
+//}
 
 QTEST_APPLESS_MAIN(test)
 

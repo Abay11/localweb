5ab27747e0dadd28fc8f3861430efb47fa9c5fef
Alim Abay <abaev.alim@yandex.ru>
2019-02-16 23:36:11 +0300

    added FTP_CLIENT implementation

--- MY_FTP/MY_FTP/ftp_client.cpp
+++ MY_FTP/MY_FTP/ftp_client.cpp
@@ -1,6 +1,52 @@
 #include "ftp_client.h"
 
-FTP_CLIENT::FTP_CLIENT()
+FtpClient::FtpClient(const QString &address)
+ :serverAddress(address)
+{}
+
+FtpClient::~FtpClient()
+{}
+
+void FtpClient::connectToServer()
+{
+ socket->connectToHost(serverAddress, port);
+}
+
+void FtpClient::disconnect()
+{
+ socket->close();
+}
+
+void FtpClient::upload(const QString &path, const QString &filename)
+{
+ QFile file(path + filename);
+
+ if(file.open(QFile::ReadOnly))
+	{
+	 QDataStream out(socket);
+	 out.setVersion(QDataStream::Qt_5_11);
+
+	 QFileInfo finfo(file);
+
+	 expectedSize=static_cast<quint64>(finfo.size());
+
+	 QByteArray buffer;
+	 buffer = file.read(100000000);
+	 socket->write(buffer);
+
+	 file.close();
+	}
+ else {
+	 qWarning()<<"FTP_CLIENT: File openning error has occurred";
+	}
+}
+
+void FtpClient::slotDownload()
 {
 
+}
+
+void FtpClient::slotError(QAbstractSocket::SocketError)
+{
+ qWarning()<<"FTP_CLIENT: Connection error has occured!";
 }
--- MY_FTP/MY_FTP/ftp_client.h
+++ MY_FTP/MY_FTP/ftp_client.h
@@ -1,11 +1,39 @@
 #ifndef FTP_CLIENT_H
 #define FTP_CLIENT_H
 
+#include "ftp_server.h"
 
-class FTP_CLIENT
+#include <QObject>
+#include <QTcpSocket>
+#include <QFile>
+#include <QFileInfo>
+
+class FtpClient : public QObject
 {
+ Q_OBJECT
+
+private:
+ QString serverAddress;
+
+ quint16 port;
+
+ quint64 expectedSize=0;
+
+ QTcpSocket *socket;
+
 public:
- FTP_CLIENT();
+ FtpClient(const QString &serverAddress);
+
+ virtual ~FtpClient();
+
+ void connectToServer();
+ void disconnect();
+
+ void upload(const QString &path, const QString &filename);
+
+public slots:
+ void slotDownload();
+ void slotError(QAbstractSocket::SocketError);
 };
 
 #endif // FTP_CLIENT_H

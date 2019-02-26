8e57dae3ff98f98b312fe161eb392064b79ff71c
Alim Abay <abaev.alim@yandex.ru>
2019-02-26 23:08:33 +0300

    added a file available statutes

--- MY_FTP/MY_FTP/ftp_common.h
+++ MY_FTP/MY_FTP/ftp_common.h
@@ -7,6 +7,9 @@
 const qint8 DOWNLOAD = 1;
 const qint8 UPLOAD = 2;
 
+const qint8 OK = 1;
+const qint8 FILE_NOT_FOUND = 2;
+
 int const BUFFER_SIZE = 5242880; // 5MB
 
 qint64 const INIT_MSG_SIZE = sizeof(char) + sizeof(QString);

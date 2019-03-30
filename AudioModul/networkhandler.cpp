652aeb3a02dfd0944cf190016361efe64885a8ae
Alim Abay <abaev.alim@yandex.ru>
2019-03-30 15:43:09 +0300

    added network realization

--- AudioModul/networkhandler.cpp
+++ AudioModul/networkhandler.cpp
@@ -1,6 +1,30 @@
 #include "networkhandler.h"
 
-NetworkHandler::NetworkHandler()
+NetworkHandler::NetworkHandler(const QString& host, quint16 port)
+ :socket(new QUdpSocket)
+ ,host(host)
+ ,port(port)
 {
+ connect(socket, SIGNAL(slotReadyRead()), SLOT(slotReadyRead()));
+}
 
+void NetworkHandler::startListen()
+{
+ socket->bind(QHostAddress::Any, port);
+}
+
+void NetworkHandler::slotWriteData(QByteArray &data)
+{
+ QByteArray compressed = qCompress(data);
+
+ socket->writeDatagram(compressed, host, port);
+}
+
+void NetworkHandler::slotReadyRead()
+{
+ QByteArray incomeData = socket->readAll();
+
+ auto decompressed = qUncompress(incomeData);
+
+ emit readyRead(decompressed);
 }
--- AudioModul/networkhandler.h
+++ AudioModul/networkhandler.h
@@ -1,11 +1,30 @@
 #ifndef NETWORKHANDLER_H
 #define NETWORKHANDLER_H
 
+#include <QUdpSocket>
 
-class NetworkHandler
+class NetworkHandler : public QObject
 {
+ Q_OBJECT
+
+ QUdpSocket* socket;
+
+ QHostAddress host;
+
+ quint16 port;
+
 public:
- NetworkHandler();
+ NetworkHandler(const QString& host, quint16 port);
+
+ void startListen();
+
+signals:
+ void readyRead(QByteArray& data);
+
+public slots:
+ void slotWriteData(QByteArray& data);
+
+ void slotReadyRead();
 };
 
 #endif // NETWORKHANDLER_H

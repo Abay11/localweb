bcfe25e95df29713de93b05a8e58a6a63d02fce7
Alim Abay <abaev.alim@yandex.ru>
2019-04-02 12:18:26 +0300

    added partial ConnectionHandler realization

--- AudioModul/connectionhandler.cpp
+++ AudioModul/connectionhandler.cpp
@@ -1,6 +1,27 @@
 #include "connectionhandler.h"
 
-ConnectionHandler::ConnectionHandler()
+ConnectionHandler::ConnectionHandler(quint16 port, const QHostAddress& host, QObject *parent)
+ :QObject (parent)
+ ,socket(new QUdpSocket)
+ ,host(host)
+ ,port(port)
+{
+}
+
+void ConnectionHandler::startListen()
+{
+ if(!socket->bind(QHostAddress::Any, port))
+	qWarning() << "Couldn't bind a port";
+}
+
+void ConnectionHandler::slotReadDataFrom()
 {
 
+}
+
+void ConnectionHandler::slotWriteDataTo(QByteArray& data)
+{
+ auto compressed = qCompress(data);
+
+ qDebug() << "Connection handler: Received data to write(compressed size:" <<compressed.size();
 }
--- AudioModul/connectionhandler.h
+++ AudioModul/connectionhandler.h
@@ -1,11 +1,36 @@
 #ifndef CONNECTIONHANDLER_H
 #define CONNECTIONHANDLER_H
 
+#include <QUdpSocket>
+//#include <
 
-class ConnectionHandler
+class ConnectionHandler : public QObject
 {
+ Q_OBJECT
+
+ private:
+ QUdpSocket* socket;
+
+ QHostAddress host;
+
+ quint16 port;
+
 public:
- ConnectionHandler();
+ explicit ConnectionHandler(quint16 port, const QHostAddress& host = QHostAddress("localhost"), QObject* parent=nullptr);
+
+ void startListen();
+
+signals:
+ void dataArrived(QByteArray&);
+
+private slots:
+ void slotReadDataFrom();
+
+public slots:
+ void slotWriteDataTo(QByteArray&);
+
+
+
 };
 
 #endif // CONNECTIONHANDLER_H

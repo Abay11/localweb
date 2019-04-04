746614c7a6f58df3afa780f54780b022a7f317d0
Alim Abay <abaev.alim@yandex.ru>
2019-04-04 16:02:52 +0300

    ServerSide realization

--- AudioModul/serverside.cpp
+++ AudioModul/serverside.cpp
@@ -1,6 +1,42 @@
 #include "serverside.h"
 
-ServerSide::ServerSide(QObject *parent) : QObject(parent)
+void ServerSide::startListening()
 {
+ if(!socket->bind(listeningPort))
+	qWarning() << DTAG + "Couldn't bind to a port!";
+}
 
+void ServerSide::insertClient(const QHostAddress& address,  quint16 port)
+{
+ connectedClients.push_back(qMakePair(address, port));
+}
+
+void ServerSide::sendIndividual(QByteArray &data, QHostAddress& address, quint16 port)
+{
+	socket->writeDatagram(data, address, port);
+}
+
+void ServerSide::sendBroadcast(QByteArray& data)
+{
+ for(auto c : connectedClients)
+	sendIndividual(data, c.first, c.second);
+}
+
+ServerSide::ServerSide(quint16 nport, QObject *parent) : QObject(parent)
+,listeningPort(nport)
+,socket(new QUdpSocket(this))
+{
+ connect(socket, SIGNAL(readyRead()), this, SLOT(slotReadyRead()));
+}
+
+void ServerSide::slotReadyRead()
+{
+ auto dataSize = socket->pendingDatagramSize();
+
+ QByteArray receivedData;
+ receivedData.resize(static_cast<int>(dataSize));
+
+ QHostAddress from_address;
+ quint16 from_port;
+ socket->readDatagram(receivedData.data(), dataSize, &from_address, &from_port);
 }
--- AudioModul/serverside.h
+++ AudioModul/serverside.h
@@ -2,14 +2,39 @@
 #define SERVERSIDE_H
 
 #include <QObject>
+#include <QUdpSocket>
+#include <QVector>
+#include <QPair>
+
+#include <QDebug>
+
+static QString DTAG("AudioModule::ServerSide:");
 
 class ServerSide : public QObject
 {
  Q_OBJECT
+private:
+ quint16 listeningPort;
+
+ QUdpSocket* socket;
+
+ QVector<QPair<QHostAddress, quint16>> connectedClients;
+
+ void startListening();
+
+ void insertClient(const QHostAddress& host, quint16 port);
+
+ void sendIndividual(QByteArray& data, QHostAddress&, quint16);
+
+ void sendBroadcast(QByteArray& data);
+
 public:
- explicit ServerSide(QObject *parent = nullptr);
+ explicit ServerSide(quint16 listeningPort, QObject *parent = nullptr);
 
 signals:
+
+private slots:
+ void slotReadyRead();
 
 public slots:
 };

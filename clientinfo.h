199c21f02e9c4d4edb7aca964f75ad1ee92fa6e0
Alim Abay <abaev.alim@yandex.ru>
2019-04-17 18:04:05 +0300

    visual refactoring

--- clientinfo.h
+++ clientinfo.h
@@ -19,8 +19,11 @@ class ClientInfo
 {
 private:
  QString mfullName;
+
  QString maddress;
+
  QString mport;
+
  bool mstatus=false;
 
 public:
@@ -28,22 +31,32 @@ public:
             QString addr=QString(),
             QString mport=QString(),
             bool status=false);
+
  virtual ~ClientInfo();
 
  const QString& fullName()const;
+
  QString& fullName();
+
  const QString& address()const;
+
  QString& address();
+
  const QString& port() const;
+
  QString& port();
+
  const bool& status() const;
+
  bool& status();
 };
 
 QDataStream& operator>>(QDataStream& out, ClientInfo *&cinfo);
+
 QDataStream& operator<<(QDataStream& out, const ClientInfo *const&cinfo);
 
-QDataStream &operator<<(QDataStream &out, DATATYPE &type);
-QDataStream &operator>>(QDataStream &out, DATATYPE &type);
+QDataStream& operator<<(QDataStream &out, DATATYPE &type);
+
+QDataStream& operator>>(QDataStream &out, DATATYPE &type);
 
 #endif // CLIENTINFO_H

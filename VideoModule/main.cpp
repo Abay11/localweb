b2f1b49cadb8418a2f6e6528c2f850851df21f8f
Alim Abay <abaev.alim@yandex.ru>
2019-05-25 11:15:56 +0300

    update main

--- VideoModule/main.cpp
+++ VideoModule/main.cpp
@@ -2,12 +2,20 @@
 
 #include "clientside.h"
 
+#include <QWidget>
+
 int main(int argc, char* argv[])
 {
 	QApplication a(argc, argv);
 
 	ClientSide* client = new ClientSide(16000);
+
+	client->setDestination(QHostAddress("127.0.0.1"),
+		6001);
+
 	client->startCamera();
+
+	client->getVideoWidget()->show();
 
 	return a.exec();
 }
